085d7a76293ba7fe07f3193ee1e72efc850be854
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-03-10 01:54:36 +0100

    modify index.html style.css main.js add images

--- main.js
+++ main.js
@@ -1,55 +1,129 @@
-const urlBase = 'https://pokeapi.co/api/v2/pokemon/'  // esta seria la url para todos, solo cambia el ultimo caracter que sera el nº de pokemon
-const myCarts = [];                                   
-const contain$$ = document.querySelector('[data-function = "contain"]')
+const myCarts = [];
+const contain$$ = document.querySelector('[data-function = "contain"]');
+const inputFind$$ = document.querySelector('[data-function="input-find"]');
+const btnFind$$ = document.querySelector('[data-function="btn-find"]');
+const inputFindB$$ = document.querySelector('[data-function="input-findb"]');
+const btnFindB$$ = document.querySelector('[data-function="btn-findb"]');
 
+// hacemos la peticion a la api
 
-// hacemos la peticion a la api 
-const getinfo = async(url)=>{
-    const rawInfo = await fetch (url);
+const getinfo = async () => {
+  for (let i = 1; i < 151; i++) {
+    const rawInfo = await fetch("https://pokeapi.co/api/v2/pokemon/" + i);
     const formatInfo = await rawInfo.json();
-    setInfo(formatInfo);//le pasamos la info a la funcion para generar nuestro objeto.
-}
-// iteramos por las 150 urls de cada poquemon y las pasamos a la funtion setInfo.
-for(let i=1; i<151; i++){
-    const url = 'https://pokeapi.co/api/v2/pokemon/'+i
-    getinfo(url);
- }
-//recogemos la informacion recibida y la guardamos en un objeto
+    myCarts.push(formatInfo);
+  }
+  const cartObj = myCarts.map((param) => ({
+    image: param.sprites.other.dream_world.front_default,
+    name: param.name,
+    order: param.order,
+    height: param.height / 10,
+    weight: param.weight / 10,
+  }));
+  setInfo(cartObj);
+};
+getinfo();
+
 const setInfo = (cards) => {
-  const cartObj = {
-    image: cards.sprites.other.dream_world.front_default,
-    name: cards.name,
-    order: cards.order,
-    height: cards.height / 10,
-    weight: cards.weight / 10,
-  };
-  myCarts.push(cartObj)
-  const{image,name,order,height,weight} = cartObj; // hago el destructuring para ahorrarme alguna palabra de código
-  const div$$ = document.createElement('div');   
-  const divTxt$$=document.createElement('div');     
-  const h3$$ = document.createElement('h3');
-  const img$$ = document.createElement('img');
-  const order$$ = document.createElement('h5');
-  const height$$ = document.createElement('h5');
-  const weight$$ = document.createElement('h5');
+  limpiarHtml();
+  for (const card of cards) {
+    const { image, name, order, height, weight } = card;
+    const div$$ = document.createElement("div");
+    const divTxt$$ = document.createElement("div");
+    const h3$$ = document.createElement("h3");
+    const img$$ = document.createElement("img");
+    const order$$ = document.createElement("h5");
+    const height$$ = document.createElement("h5");
+    const weight$$ = document.createElement("h5");
 
-  div$$.classList.add('card-contain')
-  divTxt$$.classList.add('txt-contain-card')
-  img$$.src = image;
-  h3$$.innerHTML = name;
-  order$$.innerHTML = '#'+order;
-  height$$.innerHTML = height+' m.';
-  weight$$.innerHTML = weight+' Kg.';
+    div$$.classList.add("card-contain");
+    divTxt$$.classList.add("txt-contain-card");
+    img$$.src = image;
+    h3$$.innerHTML = name;
+    order$$.innerHTML = "Pokemon  #" + order;
+    height$$.innerHTML = "Altura: " + height + " m.";
+    weight$$.innerHTML = "Peso:  " + weight + " Kg.";
 
-  div$$.appendChild(h3$$);
-  div$$.appendChild(img$$);
-  divTxt$$.appendChild(order$$);
-  divTxt$$.appendChild(height$$);
-  divTxt$$.appendChild(weight$$);
-  div$$.appendChild(divTxt$$);
+    div$$.appendChild(h3$$);
+    div$$.appendChild(img$$);
+    divTxt$$.appendChild(order$$);
+    divTxt$$.appendChild(height$$);
+    divTxt$$.appendChild(weight$$);
+    div$$.appendChild(divTxt$$);
+    contain$$.appendChild(div$$);
+  }
 
-  contain$$.appendChild(div$$);   
+  btnFind$$.addEventListener("click", () => {    
+    findCards(inputFind$$.value, cards);
+   
+  });
 
+  btnFindB$$.addEventListener("click", () => {    
+    findCards(inputFindB$$.value, cards);
+   
+  });
 };
 
-  
+function findCards(valor, cards) {
+  limpiarHtml();
+  const nav$$ = document.querySelector("header");
+  nav$$.style.display = "none";
+  for (const card of cards) {
+    if (card.name.toLowerCase() === valor.toLowerCase()){
+      const { image, name, order, height, weight } = card;
+      const div$$ = document.createElement("div");
+      const divTxt$$ = document.createElement("div");
+      const h3$$ = document.createElement("h3");
+      const img$$ = document.createElement("img");
+      const order$$ = document.createElement("h5");
+      const height$$ = document.createElement("h5");
+      const weight$$ = document.createElement("h5");
+
+      div$$.classList.add("card-containB");
+      divTxt$$.classList.add("txt-contain-cardB");
+      img$$.src = image;
+      h3$$.innerHTML = name;
+      order$$.innerHTML = "Pokemon  #" + order;
+      height$$.innerHTML = "Altura: " + height + " m.";
+      weight$$.innerHTML = "Peso:  " + weight + " Kg.";
+
+      div$$.appendChild(h3$$);
+      div$$.appendChild(img$$);
+      divTxt$$.appendChild(order$$);
+      divTxt$$.appendChild(height$$);
+      divTxt$$.appendChild(weight$$);
+      div$$.appendChild(divTxt$$);
+      contain$$.appendChild(div$$);
+      const btnback$$ = document.createElement("button");
+      btnback$$.classList.add("btn-find");
+      btnback$$.innerHTML = "Atrás";
+      div$$.appendChild(btnback$$);
+      btnback$$.addEventListener("click", () => {
+        nav$$.style.display = "inherit";
+        setInfo(cards);
+      });
+      return;
+    }
+  }
+  const div$$ = document.createElement("div");
+  const h3$$ = document.createElement("h3");
+  div$$.classList.add("card-containB");
+  h3$$.innerHTML = "POKEMON NO ENCONTRADO PRUEBA CON OTRO";
+  div$$.appendChild(h3$$);
+  contain$$.appendChild(div$$);
+
+  const btnback$$ = document.createElement("button");
+  btnback$$.classList.add("btn-find");
+  btnback$$.innerHTML = "Atrás";
+  div$$.appendChild(btnback$$);
+  btnback$$.addEventListener("click", () => {
+    nav$$.style.display = "inherit";
+    setInfo(cards);
+  });
+}
+
+function limpiarHtml() {
+  while (contain$$.firstChild) {
+    contain$$.removeChild(contain$$.firstChild);
+  }
+}
